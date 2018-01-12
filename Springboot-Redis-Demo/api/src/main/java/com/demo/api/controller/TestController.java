63b1771719cfcc39aae9d963410b778ff74e9404
liuxilin <liuxilin@thunisoft.com>
2018-01-12 08:48:13 +0800

    新增项目

---
+++ Springboot-Redis-Demo/api/src/main/java/com/demo/api/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.demo.api.controller;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestController {
+}
---
+++ Springboot-Redis-Demo/model/src/main/java/com/model/Role.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class Role {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
