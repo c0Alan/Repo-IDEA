eed2e066fbf31038458c28ce430cd756a104cc55
c0Alan <785837442@qq.com>
2018-04-02 23:25:32 +0800

    [*] 更新 Redis 模块

---
+++ spring-demo-server01/redis/src/main/java/com/redis/controller/RdUserController.java
@@ -0,0 +1,45 @@
+package com.redis.controller;
+
+import com.redis.model.User;
+import com.redis.service.UserRedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class RdUserController {
+    @Autowired
+    private UserRedisService userRedisService;
+
+    /**
+     * @return
+     * @throws
+     * @Title: UserController
+     * @Description: 初始化redis数据
+     */
+    @RequestMapping("/initRedisdata")
+    @ResponseBody
+    public String initRedisData() {
+        userRedisService.redisInitData();
+        return "success";
+    }
+
+    @RequestMapping("/getUserRedisByLoginName/{loginName}")
+    @ResponseBody
+    public Map<String, Object> getUserRedisByLoginName(@PathVariable String loginName) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        User user = userRedisService.getUserRedis(loginName);
+        Assert.notNull(user);
+        result.put("name", user.getName());
+        result.put("loginName", user.getLoginName());
+        result.put("departmentName", user.getDepartment().getName());
+        result.put("roleName", user.getRoleList().get(0).getName());
+        return result;
+    }
+}
