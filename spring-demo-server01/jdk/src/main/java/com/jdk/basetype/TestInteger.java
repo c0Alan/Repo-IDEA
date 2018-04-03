052bde3f61a66d4834e80fcd1f1bbe251c24076e
liuxilin <liuxilin@thunisoft.com>
2018-04-03 09:18:34 +0800

    更新jdk模块

---
+++ spring-demo-server01/jdk/src/main/java/com/jdk/basetype/TestInteger.java
@@ -0,0 +1,9 @@
+package com.jdk.basetype;
+
+public class TestInteger {
+    public static void main(String[] args) {
+        Integer num1 = -1;
+        int num2 = -1;
+        System.out.println(Integer.valueOf(num2).equals(num1));
+    }
+}
