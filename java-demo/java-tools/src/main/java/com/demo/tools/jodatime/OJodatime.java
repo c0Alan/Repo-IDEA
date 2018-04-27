fce8f9a31db5575fef9d7f82e18c654a9019aa72
c0Alan <785837442@qq.com>
2018-04-27 20:50:36 +0800

    更新java项目

--- java-demo/java-tools/src/main/java/com/demo/tools/jodatime/OJodatime.java
+++ java-demo/java-tools/src/main/java/com/demo/tools/jodatime/OJodatime.java
@@ -6,7 +6,6 @@ import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import edu.emory.mathcs.backport.java.util.Collections;
 
 import java.util.*;
 
