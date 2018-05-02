6d72b60b560882e3a795fe9744967a7a245ba1cf
c0Alan <785837442@qq.com>
2018-05-02 08:05:02 +0800

    更新 springmvc 模块

--- spring-demo-server01/springmvc/src/main/java/com/springmvc/controller/SpringMVCDemo2.java
+++ spring-demo-server01/springmvc/src/main/java/com/springmvc/controller/SpringMVCDemo2.java
@@ -1,4 +1,136 @@
 package com.springmvc.controller;
 
+import com.springmvc.dao.EmployeeDao;
+import com.springmvc.exception.WrongPasswordException;
+import com.springmvc.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+
+@Controller
+@RequestMapping("/demo2")
 public class SpringMVCDemo2 {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
+    @RequestMapping("/testFileUpload")
+    public String testFileUpload(@RequestParam(value = "desc", required = false) String desc,
+                                 @RequestParam(value = "file", required = false) MultipartFile file,
+                                 HttpServletRequest request) throws IOException {
+        System.out.println("desc: " + desc);
+        System.out.println(request.getParameter("desc"));
+        System.out.println("OriginalFilename: " + file.getOriginalFilename());
+        System.out.println("InputStream: " + file.getInputStream());
+        return "success";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/testHttpMessageConverter")
+    public String testHttpMessageConverter(@RequestBody String body) {
+        System.out.println(body);
+        return "hello world! " + new Date();
+    }
+
+    @RequestMapping("/testResponseEntity")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+        byte[] body = null;
+        ServletContext servletContext = session.getServletContext();
+        InputStream in = servletContext.getResourceAsStream("/WEB-INF/classes/i18n.properties");
+        body = new byte[in.available()];
+        in.read(body);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment;filename=abc.txt");
+
+        HttpStatus statusCode = HttpStatus.OK;
+
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+        return response;
+    }
+
+    @ResponseBody
+    @RequestMapping("/testJson")
+    public Collection<Employee> testJson() {
+        return employeeDao.getAll();
+    }
+
+    @RequestMapping("/i18n")
+    public String testI18n(Locale locale) {
+        String val = messageSource.getMessage("i18n.user", null, locale);
+        System.out.println(val);
+        return "i18n";
+    }
+
+
+
+    /**
+     * i = 0 时抛出的数学异常将被 ExceptionHandler 捕获处理
+     * @param i
+     * @return
+     */
+    @RequestMapping("/testExceptionHandlerExceptionResolver")
+    public String testExceptionHandlerExceptionResolver(@RequestParam("i") int i) {
+        System.out.println("result: " + (10 / i));
+        return "success";
+    }
+
+    /**
+     * i = 13 模拟抛出密码错误异常
+     * i != 13 模拟抛出404 异常
+     *
+     * @param i
+     * @return
+     */
+    @ResponseStatus(reason = "测试", value = HttpStatus.NOT_FOUND)
+    @RequestMapping("/testResponseStatusExceptionResolver")
+    public String testResponseStatusExceptionResolver(@RequestParam("i") int i) {
+        if (i == 13) {
+            throw new WrongPasswordException();
+        }
+        System.out.println("testResponseStatusExceptionResolver...");
+
+        return "success";
+    }
+
+    /**
+     * 抛出获取不到Get方法异常
+     * @return
+     */
+    @RequestMapping(value = "/testDefaultHandlerExceptionResolver", method = RequestMethod.POST)
+    public String testDefaultHandlerExceptionResolver() {
+        System.out.println("testDefaultHandlerExceptionResolver...");
+        return "success";
+    }
+
+    /**
+     * 测试数据越界异常处理
+     * @param i
+     * @return
+     */
+    @RequestMapping("/testSimpleMappingExceptionResolver")
+    public String testSimpleMappingExceptionResolver(@RequestParam("i") int i) {
+        String[] vals = new String[10];
+        System.out.println(vals[i]);
+        return "success";
+    }
 }
