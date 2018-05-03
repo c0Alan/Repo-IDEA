4a464e0a1fd13ce68dad24343d7ed149e3714ef8
liuxilin <liuxilin@thunisoft.com>
2018-05-03 12:23:43 +0800

    更新 ss 模块

--- spring-demo-server01/ss/src/main/java/com/ss/service/UserService.java
+++ spring-demo-server01/ss/src/main/java/com/ss/service/UserService.java
@@ -1,15 +1,10 @@
 package com.ss.service;
 
-import com.ss.controller.HelloWorld;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-	@Autowired
-	private HelloWorld helloWorld;
-	
 	public UserService() {
 		System.out.println("UserService Constructor...");
 	}
