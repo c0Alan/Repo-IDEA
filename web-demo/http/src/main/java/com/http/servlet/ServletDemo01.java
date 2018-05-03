1cdb5c2fee5c1e2fa68e3692c2fb9d67a490a4ed
c0Alan <785837442@qq.com>
2018-05-03 08:06:04 +0800

    新增http模块

---
+++ web-demo/http/src/main/java/com/http/servlet/ServletDemo01.java
@@ -0,0 +1,23 @@
+package com.http.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletDemo01 extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setStatus(302);//设置服务器的响应状态码
+        /**
+         *设置响应头，服务器通过 Location这个头，来告诉浏览器跳到哪里，这就是所谓的请求重定向
+         */
+        response.setHeader("Location", "/JavaWeb_HttpProtocol_Study_20140528/1.jsp");
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
