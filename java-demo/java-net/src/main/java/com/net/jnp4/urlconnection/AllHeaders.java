d8ac43026901ddc166d90b16c3b012fe97afe0e9
c0Alan <785837442@qq.com>
2018-05-09 17:58:16 +0800

    添加知识地图认定

--- java-demo/java-net/src/main/java/com/net/jnp4/urlconnection/AllHeaders.java
+++ java-demo/java-net/src/main/java/com/net/jnp4/urlconnection/AllHeaders.java
@@ -5,22 +5,22 @@ import java.net.*;
 
 public class AllHeaders {
 
-  public static void main(String[] args) {
-    for (int i = 0; i < args.length; i++) {
-      try {
-        URL u = new URL(args[i]);
-        URLConnection uc = u.openConnection();
-        for (int j = 1; ; j++) {
-          String header = uc.getHeaderField(j);
-          if (header == null) break;
-          System.out.println(uc.getHeaderFieldKey(j) + ": " + header);
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            try {
+                URL u = new URL(args[i]);
+                URLConnection uc = u.openConnection();
+                for (int j = 1; ; j++) {
+                    String header = uc.getHeaderField(j);
+                    if (header == null) break;
+                    System.out.println(uc.getHeaderFieldKey(j) + ": " + header);
+                }
+            } catch (MalformedURLException ex) {
+                System.err.println(args[i] + " is not a URL I understand.");
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+            System.out.println();
         }
-      } catch (MalformedURLException ex) {
-        System.err.println(args[i] + " is not a URL I understand.");
-      } catch (IOException ex) {
-        System.err.println(ex);
-      }
-      System.out.println();
-    } 
-  }
+    }
 }
