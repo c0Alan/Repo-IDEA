d2611b07d8d2ccc417a384b972e45d40bf5e1d7d
liuxilin <liuxilin@thunisoft.com>
2018-05-31 13:10:14 +0800

    a

---
+++ java-demo/java/src/main/java/com/jdk/modifier/finals/ParentFinalDemo.java
@@ -0,0 +1,15 @@
+package com.jdk.modifier.finals;
+
+public class ParentFinalDemo {
+
+    /**
+     * 在早期的Java实现版本中，会将final方法转为内嵌调用。
+     */
+    public final void finalMethod(){
+        System.out.println("final method can't be overwrided!");
+    }
+
+    class OtherClass extends ParentFinalDemo {
+
+    }
+}
