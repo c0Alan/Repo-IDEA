dfe5ccfc8b15f2fd264107bf26b7da57076d9643
liuxilin <liuxilin@thunisoft.com>
2018-06-11 13:12:23 +0800

    u

---
+++ java-demo/java/src/main/java/com/jdk/collection/set/demo01/Child.java
@@ -0,0 +1,12 @@
+package com.jdk.collection.set.demo01;
+
+public class Child extends Parent {
+    public Child() {
+        super(3);
+    }
+
+    public int compareTo(Object o) {
+        System.out.println("method of child");
+        return 1;
+    }
+}
