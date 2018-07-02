748bced04534b158615996727d15b8257e2d781a
liuxilin <liuxilin@thunisoft.com>
2018-07-02 17:05:13 +0800

    测试 Collections.max 方法

--- java-demo/java/src/main/java/com/jdk/collection/util/CollectionsDemo.java
+++ java-demo/java/src/main/java/com/jdk/collection/util/CollectionsDemo.java
@@ -1,12 +1,12 @@
 package com.jdk.collection.util;
 
+import com.model.Person;
+import com.sun.javafx.scene.control.skin.VirtualFlow.ArrayLinkedList;
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * java 集合工具类
@@ -33,5 +33,36 @@ public class CollectionsDemo {
         Enumeration<String> days = weekDays.elements();
         List<String> list = Collections.list(days);
         logger.info(list);
+    }
+
+    /**
+     * 获取集合最大值
+     * 
+     * @author liuxl
+     * @date 2018/7/2 11:20
+     */
+    @Test
+    public void max(){
+        Person p1 = (new Person(null));
+        Person p2 = (new Person(new DateTime("2018-06-20").toDate()));
+        Person p3 = (new Person(new DateTime("2018-07-21").toDate()));
+        List<Person> persons = new ArrayList<>();
+        persons.add(p1);
+        persons.add(p2);
+        persons.add(p3);
+        Person p = Collections.max(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if(o1.getBirthDay() == null){
+                    return -1;
+                }
+                if(o2.getBirthDay() == null){
+                    return 1;
+                }
+                return o1.getBirthDay().getTime() - o2.getBirthDay().getTime() > 0 ? 1 : -1;
+            }
+        });
+
+        System.out.println(p);
     }
 }
