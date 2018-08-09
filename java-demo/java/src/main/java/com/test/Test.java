bd821ba928f0915152440c9323223f6d74cc1fc3
liuxilin <785837442@qq.com>
2018-08-09 16:25:09 +0800

    u

--- java-demo/java/src/main/java/com/test/Test.java
+++ java-demo/java/src/main/java/com/test/Test.java
@@ -1,5 +1,10 @@
 package com.test;
 
+/**
+ *
+ * @Author liuxilin
+ * @Date 2018/8/4 10:54
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!");
