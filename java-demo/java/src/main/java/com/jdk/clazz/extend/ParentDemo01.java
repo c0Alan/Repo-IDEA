8cfe4779b7dbcdd99d2508c6fe4b0b36098ad3de
liuxilin <785837442@qq.com>
2018-08-21 19:28:08 +0800

    u

--- java-demo/java/src/main/java/com/jdk/clazz/extend/ParentDemo01.java
+++ java-demo/java/src/main/java/com/jdk/clazz/extend/ParentDemo01.java
@@ -1,6 +1,12 @@
 package com.jdk.clazz.extend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParentDemo01 {
+
+    List list = new ArrayList<>();
+
     protected String name = "parent";
 
     public String getName() {
@@ -9,5 +15,9 @@ public class ParentDemo01 {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printList(){
+        System.out.println(list);
     }
 }
