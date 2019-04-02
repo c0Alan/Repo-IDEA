4c4c952b720efe216e62f745b248c861c5dce36f
liuxilin <785837442@qq.com>
2019-04-02 09:13:09 +0800

    a

---
+++ springboot-demo/multiple-datasource/multiple-datasource-controller2/src/main/java/com/multiple/datasource/controller2/RoleController.java
@@ -0,0 +1,12 @@
+package com.multiple.datasource.controller2;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuxilin
+ * @version 2019-04-02
+ * @since
+ */
+@RestController
+public class RoleController {
+}
