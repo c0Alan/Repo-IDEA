9a679e2e86c0f58091f15f33e7d367f1f5dbf382
liuxilin <785837442@qq.com>
2019-05-04 21:08:05 +0800

    u

--- project-above-cloud/person/src/main/java/com/c0/pac/person/dao/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.c0.pac.person.dao;
-
-import com.c0.pac.person.domain.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long>{
-
-}
