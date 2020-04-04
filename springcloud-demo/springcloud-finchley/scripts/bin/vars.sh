0c8cecaf675810d5dd2cf590de06ebddbcc9e085
liuxilin <liuxilin@pcitech.com>
2020-04-04 19:30:35 +0800

    a

---
+++ springcloud-demo/springcloud-finchley/scripts/bin/vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+MODEL_VARS_VALUE="--spring.config.location=./application-prod.yml"
+JVM_VARS_VALUE="-server -Xms250m -Xmx250m -Xss256k -XX:+HeapDumpOnOutOfMemoryError"
+# JVM_VARS_VALUE="-server -XX:MetaspaceSize=1g -XX:MaxMetaspaceSize=2g -Xms4g -Xmx8g"
