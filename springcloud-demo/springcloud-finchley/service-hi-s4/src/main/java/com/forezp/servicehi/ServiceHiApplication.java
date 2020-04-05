bde1e8ce1ab7534b20939ca3fb7ab75b40192a37
liuxilin <liuxilin@pcitech.com>
2020-04-05 12:07:35 +0800

    a

--- springcloud-demo/springcloud-finchley/service-hi-s4/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ springcloud-demo/springcloud-finchley/service-hi-s4/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
@@ -21,7 +23,8 @@ public class ServiceHiApplication {
     String port;
 
     @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
         return "hi " + name + " ,i am from port:" + port;
     }
 
