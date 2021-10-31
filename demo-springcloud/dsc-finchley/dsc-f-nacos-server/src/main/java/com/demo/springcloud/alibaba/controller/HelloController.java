9b1e86631f2d36486d2fbfb4482cd4c4a016c1e1
liuxilin <liuxilin@pcitech.com>
2021-10-31 16:07:10 +0800

    u

--- demo-springcloud/dsc-finchley/dsc-f-nacos-server/src/main/java/com/demo/springcloud/alibaba/controller/HelloController.java
+++ demo-springcloud/dsc-finchley/dsc-f-nacos-server/src/main/java/com/demo/springcloud/alibaba/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.demo.springcloud.alibaba.controller;
 
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "测试微服务调用服务端")
 @RestController
 @Slf4j
+@RequestMapping("/helloService")
 public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam String name) {
-        log.info("invoked name = " + name);
-        return "hello " + name;
+        log.info("invoked hello1，name = " + name);
+        return "hello1 " + name;
+    }
+
+    @GetMapping("/hello2")
+    public String hello2(@RequestParam String name) {
+        log.info("invoked hello1，name = " + name);
+        return "hello2 " + name;
     }
 }
