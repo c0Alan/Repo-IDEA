956f6c5758082d8671f8ae13f8aa4183276c951d
c0Alan <785837442@qq.com>
2022-05-18 22:55:41 +0800

    u

--- demo-springcloud/dsc-hoxton/dsc-h-etcd/src/main/java/com/demo/springcloud/service/EtcdService.java
+++ demo-springcloud/dsc-hoxton/dsc-h-etcd/src/main/java/com/demo/springcloud/service/EtcdService.java
@@ -26,6 +26,43 @@ public class EtcdService {
         ByteSequence bKey = ByteSequence.from(key.getBytes());
         CompletableFuture<GetResponse> getFuture = kvClient.get(bKey);
         GetResponse response = getFuture.get();
+        log.info("response: {}", response);
         return response.toString();
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        testEtcd();
+    }
+
+    public static void testEtcd() throws ExecutionException, InterruptedException {
+        // create client using endpoints
+        Client client = Client.builder().endpoints("http://172.25.20.57:2379").build();
+
+        KV kvClient = client.getKVClient();
+        ByteSequence key = ByteSequence.from("test_key".getBytes());
+        ByteSequence value = ByteSequence.from("test_value".getBytes());
+
+        // put the key-value
+        kvClient.put(key, value).get();
+
+        // get the CompletableFuture
+        CompletableFuture<GetResponse> getFuture = kvClient.get(key);
+
+        // get the value from CompletableFuture
+        GetResponse response = getFuture.get();
+
+        System.out.println("查询结果: " + response);
+
+        ByteSequence key2 = ByteSequence.from("foo".getBytes());
+        getFuture = kvClient.get(key2);
+        response = getFuture.get();
+
+        System.out.println("查询结果2: " + response);
+
+
+        // delete the key
+//        kvClient.delete(key).get();
+
+        client.close();
     }
 }
