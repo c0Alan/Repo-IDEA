eb751d4516307cec7e9b49297ba91330294abfb2
liuxilin <liuxilin@pcitech.com>
2023-07-05 19:48:49 +0800

    a

---
+++ springcloud-hoxton-demo/sch-commons/src/main/java/com/demo/springcloud/entity/Foo.java
@@ -0,0 +1,20 @@
+package com.demo.springcloud.entity;
+
+import cn.hutool.core.bean.BeanUtil;
+import lombok.Data;
+
+/**
+ * @author liuxilin
+ * @date 2022年02月12日 22:40
+ */
+@Data
+public class Foo {
+    private Integer size = 10;
+    private Boolean sample = true;
+    private String name = "Tom";
+
+    public Object get(String attr) {
+        return BeanUtil.getFieldValue(this, attr);
+    }
+
+}
---
+++ springcloud-hoxton-demo/sch-demo/src/main/java/com/demo/springcloud/SchDemo.java
@@ -0,0 +1,14 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SchDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchDemo.class, args);
+    }
+}
