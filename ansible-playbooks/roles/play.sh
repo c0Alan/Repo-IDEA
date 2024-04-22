bc2476c9abeb974454e4c2c54f0cc63ff0e89eb9
c0Alan <785837442@qq.com>
2024-04-22 23:27:58 +0800

    a

--- ansible-playbooks/roles/play.sh
+++ ansible-playbooks/roles/play.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 echo "服务器初始化"
-ansible-playbook -i hosts init.yml
+ansible-playbook -i hosts init.yml -vv
 
 echo ""
 echo "安装 jdk"
-ansible-playbook -i hosts jdk.yml
+ansible-playbook -i hosts jdk.yml -vv
 
 echo ""
 echo "安装 nginx"
-ansible-playbook -i hosts nginx.yml
+ansible-playbook -i hosts nginx.yml -vv
+
+echo ""
+echo "安装 mysql"
+ansible-playbook -i hosts mysql.yml -vv
+
+echo ""
+echo "安装 nacos"
+ansible-playbook -i hosts nacos.yml -vv
+
+
 
