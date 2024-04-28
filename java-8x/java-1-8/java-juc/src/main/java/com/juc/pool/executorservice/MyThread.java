8537ae0e9cb9db483550cedd2ca652bf134c026f
c0Alan <785837442@qq.com>
2024-04-28 22:53:52 +0800

    d

--- java-8x/java-1-8/java-juc/src/main/java/com/juc/pool/executorservice/MyThread.java
+++
@@ -1,9 +0,0 @@
-package com.juc.pool.executorservice;
-
-public class MyThread extends Thread {
-
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName()+ " is running.");
-    }
-}
