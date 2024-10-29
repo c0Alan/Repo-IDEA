382c990451e79816e8d005f3d7f8f47315e10dd2
c0Alan <785837442@qq.com>
2024-10-29 22:33:23 +0800

    u

--- springcloud-hoxton/sch-springboot/src/main/java/com/demo/springcloud/controller/ElasticsearchController.java
+++ springcloud-hoxton/sch-springboot/src/main/java/com/demo/springcloud/controller/ElasticsearchController.java
@@ -5,10 +5,14 @@ import com.demo.springcloud.repository.ElasticsearchPhoneRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,13 +30,19 @@ public class ElasticsearchController {
     @Autowired
     private ElasticsearchPhoneRepository phoneRepository;
 
-    @Autowired
+    @Resource
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
 
-    //插入数据
+    /**
+     * 插入数据
+     */
     @GetMapping("/save")
-    public void save() {
-        ElasticsearchPhone phone = new ElasticsearchPhone(1, "诺基亚5300", "诺基亚");
+    public void save() throws IOException {
+        ElasticsearchPhone phone = new ElasticsearchPhone(1, "诺基亚", "诺基亚5300");
+//        IndexRequest indexRequest = new IndexRequest("idx_phone", "_doc").id(phone.getId() + "").source(phone, XContentType.JSON);
+//        elasticsearchRestTemplate.getClient().index(indexRequest, RequestOptions.DEFAULT);
+        IndexQuery indexQuery = new IndexQueryBuilder().withObject(phone).build();
+        elasticsearchRestTemplate.index(indexQuery);
     }
 
     //批量插入数据
