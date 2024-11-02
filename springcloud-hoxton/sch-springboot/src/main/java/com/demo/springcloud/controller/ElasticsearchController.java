b191c77d147587eb4b6753787c9c383ed9658ce2
c0Alan <785837442@qq.com>
2024-11-02 21:33:55 +0800

    u

--- springcloud-hoxton/sch-springboot/src/main/java/com/demo/springcloud/controller/ElasticsearchController.java
+++ springcloud-hoxton/sch-springboot/src/main/java/com/demo/springcloud/controller/ElasticsearchController.java
@@ -5,14 +5,13 @@ import com.demo.springcloud.repository.ElasticsearchPhoneRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,24 +35,67 @@ public class ElasticsearchController {
     /**
      * 插入数据
      */
-    @GetMapping("/save")
-    public void save() throws IOException {
-        ElasticsearchPhone phone = new ElasticsearchPhone(1, "诺基亚", "诺基亚5300");
-//        IndexRequest indexRequest = new IndexRequest("idx_phone", "_doc").id(phone.getId() + "").source(phone, XContentType.JSON);
-//        elasticsearchRestTemplate.getClient().index(indexRequest, RequestOptions.DEFAULT);
+    @PostMapping("/save")
+    public void save(@RequestBody ElasticsearchPhone phone) {
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(phone).build();
         elasticsearchRestTemplate.index(indexQuery);
     }
+
 
     //批量插入数据
     @GetMapping("/batchSave")
     public void batchSave() {
         List<ElasticsearchPhone> list = new ArrayList<>();
-        ElasticsearchPhone phone2 = new ElasticsearchPhone(2, "小米", "小米12pro");
-        ElasticsearchPhone phone3 = new ElasticsearchPhone(3, "苹果", "iphone12");
+        ElasticsearchPhone phone2 = new ElasticsearchPhone(5, "小米", "小米5pro");
+        ElasticsearchPhone phone3 = new ElasticsearchPhone(6, "苹果", "iphone6");
+        IndexQueryBuilder indexQueryBuilder = new IndexQueryBuilder();
+        List<IndexQuery> indexQueryList = new ArrayList<>();
+        indexQueryList.add(indexQueryBuilder.withObject(phone2).build());
+        indexQueryList.add(indexQueryBuilder.withObject(phone3).build());
+        elasticsearchRestTemplate.bulkIndex(indexQueryList);
+    }
+
+    //批量插入数据
+    @GetMapping("/batchSave2")
+    public void batchSave2() {
+        List<ElasticsearchPhone> list = new ArrayList<>();
+        ElasticsearchPhone phone2 = new ElasticsearchPhone(2, "小米", "小米2pro");
+        ElasticsearchPhone phone3 = new ElasticsearchPhone(3, "苹果", "iphone3");
         list.add(phone2);
         list.add(phone3);
         phoneRepository.saveAll(list);
     }
+
+    //修改数据
+    @PostMapping("/update")
+    public void update(@RequestBody ElasticsearchPhone phone){
+        IndexQuery indexQuery = new IndexQueryBuilder().withObject(phone).build();
+        elasticsearchRestTemplate.index(indexQuery);
+    }
+
+    //删除数据
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable String id){
+        elasticsearchRestTemplate.delete(ElasticsearchPhone.class, id);
+    }
+
+    /**
+     * 删除id为id的数据
+     */
+    @GetMapping("/delete2/{id}")
+    public void delete2(@PathVariable long id){
+        phoneRepository.deleteById(id);
+    }
+
+    /**
+     * 删除id大于gtId的数据
+     */
+    @GetMapping("/deleteByCriteriaQuery")
+    public void deleteByCriteriaQuery(@RequestParam int gtId){
+        Criteria criteria = Criteria.where("id").greaterThan(gtId);
+        CriteriaQuery criteriaQuery = new CriteriaQuery(criteria);
+        elasticsearchRestTemplate.delete(criteriaQuery, ElasticsearchPhone.class);
+    }
+
 
 }
